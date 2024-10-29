d9275852e0648cd68e8fd64cb0bd5dce1ddc4dde
Юлия <veberulia530@gmail.com>
2024-10-29 20:12:13 +0300

    Add files via upload

---
+++ modyl_1_4.py
@@ -0,0 +1,10 @@
+name = input ("Напишите полностью фамилию,имя,отчество ? ")
+len(name)
+print(len(name))
+print(name.upper())
+print(name .lower())
+print(name.replace(" ", ""))
+print(name [0])
+print(name [-1])
+
+
